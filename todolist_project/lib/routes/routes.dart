fb8e90a6dc8b35e16c1710461bc1b0b5a4dd43f9
aqilzamzamy <aqilzamzamy@gmail.com>
2025-09-10 09:42:04 +0700

    fix [AZ] fix profile-page 2

--- todolist_project/lib/routes/routes.dart
+++ todolist_project/lib/routes/routes.dart
@@ -1,5 +1,5 @@
-class AppRoutes{
-  static const String homeFragment='/homeFragment';
-  static const String historyFragment='/historyFragment';
-  static const String profileFragment='/profileFragment';
+class AppRoutes {
+  static const String homeFragment = '/homeFragment';
+  static const String historyFragment = '/historyFragment';
+  static const String profileFragment = '/profileFragment';
 }
